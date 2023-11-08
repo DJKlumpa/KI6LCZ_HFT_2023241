5e8b1e8f80bb417373efc65064aa51ca564678e0
Barnabás Géczy <33030158+DJKlumpa@users.noreply.github.com>
2023-11-08 23:45:18 +0100

    Add files via upload

---
+++ KI6LCZ_HFT_2023241/KI6LCZ_HFT_2023241.Repository/MusicDbContext.cs
@@ -0,0 +1,33 @@
+﻿using KI6LCZ_HFT_2023241.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace KI6LCZ_HFT_2023241.Repository
+{
+    public interface IMusicRepository
+    {
+        void Create(Music music);
+        Music Listen(int id);
+        IQueryable<Music> ReadAll();
+        void Update (Music music);
+        void Delete(int id);
+    }
+    public class MusicDbContext : DbContext
+    {
+        DbSet<Music> Musics { get; set; }
+        DbSet<Album> Albums { get; set; }
+        DbSet<Band> Bands { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if(!builder.IsConfigured)
+            {
+                string temp = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\music.mdf;Integrated Security=True;MultipleActiveResultSets=true";
+                builder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(temp);
+            }
+        }
+    }
+}
